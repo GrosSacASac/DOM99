02fd9cb894de9344af2e56951a5937a75a3dd2ef
Walle Cyril <capocyril@hotmail.com>
2016-04-17 18:28:14 +0200

    learn JavaScript

---
+++ examples/learnjavascript/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Use a shorter name
+const D = dom99;
+// -- Hello World --
+
+D.vr.a = "";
+D.vr.b = "";
+D.vr.result = "";
+
+D.fx.change = function() {
+    //program here
+}
+
+D.linkJsAndDom();
