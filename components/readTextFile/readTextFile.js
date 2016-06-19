fb24e8f7e38ee7e973e491d740a8c92c25d28162
Walle Cyril <capocyril@hotmail.com>
2016-06-19 23:05:17 +0200

    readTextFile component now with const readTextFile = require("../../node_modules/dom99/components/readTextFile/readTextFile.js");

--- components/readTextFile/readTextFile.js
+++ components/readTextFile/readTextFile.js
@@ -1,43 +1,47 @@
 //readTextFile.js
+//browserify version
 /*jslint
     es6, maxerr: 15, browser, devel, fudge, maxlen: 100
 */
 /*global
-    dom99, FileReader, Promise
+    FileReader, Promise
 */
-const readTextFile = (function (D) {
-    "use strict";
+"use strict";
+const D = require("dom99");
 
-    const fileInputDescription = {
-        "tagName": "input",
-        "type": "file",
-        "data-fx": "xReadFileStart"
-    };
+const fileInputDescription = {
+    "tagName": "input",
+    "type": "file",
+    "data-fx": "xReadFileStart"
+};
 
-    const readerOnLoadPrepare = function (inputElement) {
-        return function (event) {
-            inputElement.remove();
-            inputElement.readFileResolve(event.target.result);
-        };
+const readerOnLoadPrepare = function (inputElement) {
+    return function (event) {
+        inputElement.remove();
+        inputElement.readFileResolve(event.target.result);
     };
+};
 
-    D.fx.xReadFileStart = function (event) {
-        const fileObject = event.target.files[0]; // FileList object
-        const reader = new FileReader();
-        reader.onload = readerOnLoadPrepare(event.target);
-        reader.readAsText(fileObject);
-    };
+D.fx.xReadFileStart = function (event) {
+    const fileObject = event.target.files[0]; // FileList object
+    const reader = new FileReader();
+    reader.onload = readerOnLoadPrepare(event.target);
+    reader.readAsText(fileObject);
+};
 
-    const readFileSetup = function (resolve, reject) {
-        const fileInput = D.createElement2(fileInputDescription);
-        fileInput.readFileResolve = resolve;
-        fileInput.readFileReject = reject;
-        D.linkJsAndDom(fileInput);
-        D.el.readTextFileContainer.appendChild(fileInput);
-        fileInput.click();
-    };
+const readFileSetup = function (resolve, reject) {
+    const fileInput = D.createElement2(fileInputDescription);
+    fileInput.readFileResolve = resolve;
+    fileInput.readFileReject = reject;
+    D.linkJsAndDom(fileInput);
+    D.el.readTextFileContainer.appendChild(fileInput);
+    fileInput.click();
+};
 
-    return function () {
-        return new Promise(readFileSetup);
-    };
-}(dom99));
+const readTextFile = function () {
+    return new Promise(readFileSetup);
+};
+
+module.exports = {
+    readTextFile
+};
