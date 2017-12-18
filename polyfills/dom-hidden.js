a1f72963222bc6b3de7af466860e7e566ba22511
Walle Cyril <capocyril@hotmail.com>
2017-12-18 18:41:32 +0100

    more solid dom-hidden

--- polyfills/dom-hidden.js
+++ polyfills/dom-hidden.js
@@ -3,9 +3,15 @@
 const bodyHidden = document.body.hidden;
 if (bodyHidden !== true && bodyHidden !== false) {
     // no support
+    const privateHidden = "_hidden";
     Object.defineProperty(HTMLElement.prototype, "hidden", {
         get: function get() {
-            return this._hidden || false;
+            if (Object.prototype.hasOwnProperty.call(this, privateHidden)) {
+                return this[privateHidden];
+            } else if (this.hasAttribute("hidden")) {
+                return true;
+            }
+            return false;
         },
         set: function set(isHidden) {
             this._hidden = isHidden;
