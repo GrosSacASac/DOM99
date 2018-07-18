926a115b51825c3fc0b1b67c4c46dfdd75de7590
Cyril Walle <capocyril@hotmail.com>
2018-07-18 22:13:38 +0200

    working on performance experiments

--- source/parentIdFromEvent.js
+++ source/parentIdFromEvent.js
@@ -6,5 +6,19 @@ export {parentIdFromEvent};
 @return {string} dataId string
 */
 const parentIdFromEvent = function (event) {
+	const target = event.target;
+	
+	return idFromNode(target);
+};
+
+
+const idFromNode = function (node) {
+	if (node.hasAttribute(`data-id`)) {
+		return node.getAttribute(`data-id`);
+	}
+	const parent = node.parentNode;
+	if (parent) {
+		return idFromNode(parent);
+	}
 	return ``;
 };
