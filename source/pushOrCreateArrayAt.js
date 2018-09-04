62b8aac4fc282526d67a3e6b0b1bf454e17b616d
Cyril Walle <capocyril@hotmail.com>
2018-09-04 19:43:23 +0200

    factory dom99create.js

---
+++ source/pushOrCreateArrayAt.js
@@ -0,0 +1,14 @@
+export {pushOrCreateArrayAt};
+
+const pushOrCreateArrayAt = (object, key, valueToPush) => {
+  // don't need to use hasOwnProp as there is no array in the prototype
+  // but still use it to avoid a warning
+  // const potentialArray = object[key]
+	if (hasOwnProperty.call(object, key)) {
+		// eventually the if is always true
+		object[key].push(valueToPush);
+	} else {
+		// only for the first time
+		object[key] = [valueToPush];
+	}
+};
