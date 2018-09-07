42ac1befcc0a7f2820728c3c7ea5cfb8d2884f21
Cyril Walle <capocyril@hotmail.com>
2018-09-07 18:23:30 +0200

    rewrite tests

--- tests/specification/spec/dom99Basics.js
+++ tests/specification/spec/dom99Basics.js
@@ -1,58 +1,57 @@
+import * as d from "../../../source/dom99.js";
 describe("DOM99 basics", function() {
-  var D = dom99;
-
-  var fakeBody = document.createElement("div");
-  fakeBody.innerHTML = '' +
-    '<div id="div1" data-element="div1"></div>' +
-    '<input data-element="input1" data-variable="string1">' +
-    '<div data-element="div2" data-function="click-functionX"></div>';
+  
+  let myfunction = function () {};
+  const content = document.createElement("div");
+  content.innerHTML = `
+    <div id="div1" data-element="div1"></div>
+    <input data-element="input1" data-variable="string1">
+    <div data-element="div2" data-function="click-functionX"></div>`;
     
-  D.activate(fakeBody);
-    
+  d.functions.functionX = function (...args) {
+	myfunction(...args);
+  };
+  d.activate(content);
+  window.d = d;  
 
   it("this test suite uses querySelector", function() {
-    expect(fakeBody.querySelector).toBeDefined();
+    expect(content.querySelector).toBeDefined();
   });
   
   it("data-element should be able to preselect an element", function() {
-    expect(D.el.div1).toEqual(fakeBody.querySelector("#div1"));
+    expect(d.elements.div1).toEqual(content.querySelector("#div1"));
   });  
   
   it("data-variable should be able to set D.vr.string1 and see the change in the dom", function() {
-    var anythingText = "abwxyz \\ \" \' <html> <script>http // %20 blabla";
-    D.vr.string1 = anythingText;
-    expect(anythingText).toEqual(D.el.input1.value);
-    //also fake input
+    const text = "abwxyz \\ \" \' <html> <script>http // %20 blabla";
+    d.feed('string1', text);
+    expect(text).toEqual(d.elements.input1.value);
   });
   
-  it("data-variable should be able to see the change in D.vr.string1 if an user input event is fired", function() {
-    var anythingText = "abwxyz \\ \" \' <html> <script>http // %20 blabla";
+  it("data-variable should be able to see the change if an user input event is fired", function() {
+    const text = "abwxyz \\ \" \' <html> <script>http // %20 blabla";
     
-
-    // Create the fake event.
-    D.el.input1.value = anythingText;
-    var event = document.createEvent('Event');
+    d.elements.input1.value = text;
+    const event = document.createEvent('Event');
     event.initEvent('input', true, true);
-    D.el.input1.dispatchEvent(event);
+    d.elements.input1.dispatchEvent(event);
     
-    expect(anythingText).toEqual(D.vr.string1);
+    expect(text).toEqual(d.get('string1'));
     
   });
   
   it("data-function should be able fire a function if the event occurs", function() {
-    var a = 0;
-    var functionX = function(event) {
-        a = 10;
-    }
+    let a = 0;
+	const finalValue = 10;
+    myfunction = function(event) {
+        a = finalValue;
+    };
     
-    D.fx.functionX = functionX;
-    
-    // Create the fake event.
-    var event = document.createEvent('Event');
+    const event = document.createEvent('Event');
     event.initEvent('click', true, true);
-    D.el.div2.dispatchEvent(event);
+    d.elements.div2.dispatchEvent(event);
     
-    expect(a).toEqual(10);//functionX has been called
+    expect(a).toEqual(finalValue);
     
   });
 
