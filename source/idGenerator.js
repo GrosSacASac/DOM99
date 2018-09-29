e3846bcedc0e3c051a77dd33c0bcc9be3c0a19e2
Walle Cyril <cyril.walle@protonmail.com>
2018-09-29 20:29:44 +0200

    make prefix optional

--- source/idGenerator.js
+++ source/idGenerator.js
@@ -1,16 +1,12 @@
-/*idGenerator()
-
-generates a predictable new id each time
-perfect for DOM id requirements
-*/
-
 export {idGenerator};
-
-const prefix = `id-`;
 
 let next = Number.MAX_SAFE_INTEGER;
 
-const idGenerator = () => {
+/*
+generates a predictable new id each time
+perfect for DOM id requirements
+*/
+const idGenerator = (prefix = ``) => {
 	const id = `${prefix}${next}`;
 	next -= 1;
 	return id;
