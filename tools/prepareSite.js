fc7f862608ce3f565e3b2ec5832fcd9d49667930
Walle Cyril <cyril.walle@protonmail.com>
2018-10-13 22:37:12 +0200

    fix website

--- tools/prepareSite.js
+++ tools/prepareSite.js
@@ -9,6 +9,7 @@ const docdeps = `./documentation/deps/`;
 const modules = `./node_modules/`;
 const inputsOutputs = {
     [`${modules}template-mb/template.js`]: `${docdeps}template.js`,
+    [`./polyfills/template-mb-bootstrap.js`]: `${docdeps}template-mb-bootstrap.js`,
     [`${modules}@babel/polyfill/dist/polyfill.min.js`]: `${docdeps}polyfill.min.js`,
     [`./polyfills/built/remove.js`]: `${docdeps}remove.js`,
     [`./built/dom99Script.es5.min.js`]: `${docdeps}dom99Script.es5.min.js`,
