be8f151a789eb01d2ba4c15a8ae7460313e702a2
Walle Cyril <cyril.walle@protonmail.com>
2019-01-30 23:12:17 +0100

    remove year
    2019

--- tools/rollup.config.js
+++ tools/rollup.config.js
@@ -12,7 +12,7 @@ const GLOBAL_NAME = `d`;
 	// advanced output options
 	// paths: {},
 	banner : `/* dom99 v${VERSION} */
-	/*        Copyright Cyril Walle 2018.
+	/*        Copyright Cyril Walle
 Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE.txt or copy at
          https://www.boost.org/LICENSE_1_0.txt */
