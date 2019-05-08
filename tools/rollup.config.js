0563cb3d4865d968e829d39c68dc62af3fc95cc8
Walle Cyril <cyril.walle@protonmail.com>
2019-05-08 22:39:30 +0200

    remove unused comments

--- tools/rollup.config.js
+++ tools/rollup.config.js
@@ -4,87 +4,52 @@ import __package from "../package.json";
 const VERSION = __package.version;
 const GLOBAL_NAME = `d`;
 
-	const commonOutputOptions = {
-	// core output options
-	name : GLOBAL_NAME,
-	// globals: [],
-
-	// advanced output options
-	// paths: {},
-	banner : `/* dom99 v${VERSION} */
+const commonOutputOptions = {
+    name: GLOBAL_NAME,
+    banner: `/* dom99 v${VERSION} */
 	/*        Copyright Cyril Walle
 Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE.txt or copy at
          https://www.boost.org/LICENSE_1_0.txt */
 `,
-	// footer: ``,
-	// intro: ``,
-	// outro: ``,
-	// sourcemap,
-	// sourcemapFile,
-	interop: false,
-	extend: false,
-
-	// danger zone
-	// exports,
-	// indent,
-	strict: true,
-	// freeze,
-	namespaceToStringTag: false
-
-	// experimental
-	// entryFileNames,
-	// chunkFileNames,
-	// assetFileNames
+    // footer: ``,
+    // intro: ``,
+    // outro: ``,
+    interop: false,
+    extend: false,
+    strict: true,
+    namespaceToStringTag: false
 };
 
-export default { // can be an array (for multiple inputs)
-  // core input options
-  input: `source/dom99.js`,     // required
-  // external: [],
-  //plugins: [],
+export default {
+    input: `source/dom99.js`,
+    treeshake: {
+        pureExternalModules: false,
+        propertyReadSideEffects: false // assume reading properties has no side effect
+    },
+    output: [
+        Object.assign({
+            format: `iife`,
+            file: `built/dom99Script.js`,
+        }, commonOutputOptions),
+        Object.assign({
+            format: `cjs`,
+            file: `built/dom99Require.js`,
+        }, commonOutputOptions),
+        Object.assign({
+            format: `amd`,
+            file: `built/dom99Amd.js`,
+            amd: {
+                id: GLOBAL_NAME
+            }
+        }, commonOutputOptions),
+        Object.assign({
+            format: `es`,
+            file: `built/dom99ES.js`
+        }, commonOutputOptions),
+    ],
 
-  // advanced input options
-  // onwarn,
-  // perf,
-
-  // danger zone
-  // acorn,
-  // acornInjectPlugins,
-  treeshake: {
-	  pureExternalModules: false,
-	  propertyReadSideEffects: false // assume reading properties has no side effect
-  },
-  // context,
-  // moduleContext,
-
-
-  output: [  // required (can be an array, for multiple outputs)
-	Object.assign({
-		format: `iife`,
-		file : `built/dom99Script.js`,
-	}, commonOutputOptions),
-	Object.assign({
-		format: `cjs`,
-		file : `built/dom99Require.js`,
-	}, commonOutputOptions),
-	Object.assign({
-		format: `amd`,
-		file : `built/dom99Amd.js`,
-		amd: {
-			id: GLOBAL_NAME
-		}
-	}, commonOutputOptions),
-	Object.assign({
-		format: `es`,
-		file : `built/dom99ES.js`
-	}, commonOutputOptions),
-  ],
-
-  watch: {
-    // chokidar,
-    // include,
-    // exclude,
-    clearScreen: true
-  }
+    watch: {
+        clearScreen: true
+    }
 };
